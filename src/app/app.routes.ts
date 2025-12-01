eca69efc03ceba58277790df4f328044175a71d5
DavMunHer <davidmunyozh@gmail.com>
2025-12-01 12:52:33 +0100

    feat: added section title component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProjectSectionComponent } from './components/project-section/project-section.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: ProjectSectionComponent,
+        path: "project-section",
+    }
+];
--- src/app/components/project-section/project-section.component.ts
+++ src/app/components/project-section/project-section.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SectionTitleComponent } from './section-title/section-title.component';
 
 @Component({
   selector: 'app-project-section',
-  imports: [],
+  imports: [SectionTitleComponent],
   templateUrl: './project-section.component.html',
   styleUrl: './project-section.component.css'
 })
