e63518fc91df432c7eb7a6c3dfe8761a0e576053
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-12-02 12:31:26 +0100

    feat: Added the main structure to the sidebar component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
+  protected toggleDropdownVal = signal<Boolean>(false)
+
+  toggleDropdown() {
+    this.toggleDropdownVal.set(!this.toggleDropdownVal());
+  }
 }
