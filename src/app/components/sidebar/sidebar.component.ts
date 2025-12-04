162bc4f80056526afe70491da3669fd997718dbc
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-12-04 12:02:08 +0100

    feat: Added the respective content on each of the menu sections

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -14,4 +14,23 @@ export class SidebarComponent {
   toggleDropdown() {
     this.toggleDropdownVal.set(!this.toggleDropdownVal());
   }
+
+  colors = signal(["#7AD2D2","#3A6FAF","#EC7580","#9BE8D8","#FFC04A","#6DAA9F","#FFF3D9","#F56A62","#C4D291"])
+  projects = signal([
+    {
+      name: 'DAM',
+      favorite: false,
+      pendingTasks: 3
+    },
+    {
+      name: 'DAW',
+      favorite: false,
+      pendingTasks: 0
+    },
+    {
+      name: 'IA BigData',
+      favorite: true,
+      pendingTasks: 6
+    }
+  ])
 }
