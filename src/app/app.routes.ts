e12add5101268dc6532570b126918683013b3487
DavMunHer <davidmunyozh@gmail.com>
2025-12-04 23:22:19 +0100

    refactor: created new subcomponent and its basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ProjectSectionComponent } from './components/project-section/project-section.component';
+import { MenuSectionComponent } from './components/sidebar/menu-section/menu-section.component';
 
 export const routes: Routes = [
-    {
-        component: ProjectSectionComponent,
-        path: "project-section",
-    }
+  {
+    component: ProjectSectionComponent,
+    path: 'project-section',
+  },
+  {
+    component: MenuSectionComponent,
+    path: 'deleteme',
+  },
 ];
