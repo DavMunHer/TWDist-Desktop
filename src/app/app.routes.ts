df0832e82bef8c3fa7d590da4dfc7383063f1258
DavMunHer <davidmunyozh@gmail.com>
2025-12-05 21:55:14 +0100

    fix: removed unused route from the router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,4 @@
 import { Routes } from '@angular/router';
-import { ProjectViewComponent } from './components/project-view/project-view.component';
 
 export const routes: Routes = [
-    // FIXME: Delete below path, since it was only for debugging purposes
-    {
-        component: ProjectViewComponent,
-        path: "project-view",
-    }
 ];
