b2f69a14bfdf3e0d1d38879c3fb77652eae82579
DavMunHer <davidmunyozh@gmail.com>
2025-12-12 19:42:31 +0100

    feat: now sending form with intro key directly

--- src/app/components/project-view/section-adder/section-adder.component.ts
+++ src/app/components/project-view/section-adder/section-adder.component.ts
@@ -1,21 +1,23 @@
 import { Component, output, signal } from '@angular/core';
 import { AutoFocusDirective } from '../../../directives/auto-focus.directive';
 import { TWDSection } from '../../../types/section';
-import { ReactiveFormsModule, FormControl } from '@angular/forms'
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'section-adder',
   imports: [AutoFocusDirective, ReactiveFormsModule],
   templateUrl: './section-adder.component.html',
-  styleUrl: './section-adder.component.css'
+  styleUrl: './section-adder.component.css',
 })
 export class SectionAdderComponent {
-  protected showSectionForm = signal<boolean>(false)
-  public onNewSectionCreated = output<TWDSection>()
+  protected showSectionForm = signal<boolean>(false);
+  public onNewSectionCreated = output<TWDSection>();
 
-  protected newSectionName = new FormControl('', {nonNullable: true});
-  protected newSectionSig = toSignal(this.newSectionName.valueChanges, {initialValue: ''})
+  protected newSectionNameCtrl = new FormControl('', { nonNullable: true });
+  protected newSectionSig = toSignal(this.newSectionNameCtrl.valueChanges, {
+    initialValue: '',
+  });
 
   protected handleClick() {
     this.showSectionForm.set(true);
@@ -23,22 +25,23 @@ export class SectionAdderComponent {
 
   protected openSectionForm() {
     // This will also hid the current "Add section" button, being replaced by the section form
-    this.showSectionForm.set(true)
+    this.showSectionForm.set(true);
   }
-  
+
   protected closeSectionForm() {
     // When section form is closed, the add section button appears again!
     this.showSectionForm.set(false);
   }
 
-  protected handleFormSubmission() {
-    // TODO: Send info of the form to the backend
+  protected handleFormSubmission(event: Event) {
+    event.preventDefault() // For not reloading the page when sending form
+    // TODO: Send info of the form to the backend and show feedback to the user when something went wrong
     const newSection: TWDSection = {
       name: this.newSectionSig(),
-      tasksList: []
-    }
+      tasksList: [],
+    };
     this.onNewSectionCreated.emit(newSection);
-    this.closeSectionForm()
+    this.newSectionNameCtrl.reset();
+    this.closeSectionForm();
   }
-
 }
