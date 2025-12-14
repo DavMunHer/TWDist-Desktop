0ecde6e8022ea3403f422fc78ce2d2ead18ed942
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-12-14 17:22:45 +0100

    feat(style): Improved the structure of profile modal component and added styles

--- src/app/components/modal/profile/profile.component.ts
+++ src/app/components/modal/profile/profile.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'profile-modal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
 
+  name = 'Oscar';
+  email = 'oscar@gmail.com';
+  oldPassword = '';
+  newPassword = '';
+
+  changePicture() {
+    console.log('Change picture clicked');
+  }
+
+  deletePicture() {
+    console.log('Delete picture clicked');
+  }
+
+  updateName() {
+    console.log('Updated name:', this.name);
+  }
+
+  updateEmail() {
+    console.log('Updated email:', this.email);
+  }
+
+  updatePassword() {
+    console.log('Old:', this.oldPassword, 'New:', this.newPassword);
+  }
+
+  deleteAccount() {
+    console.log('Account deleted');
+  }
 }
