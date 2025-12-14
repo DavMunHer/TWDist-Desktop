a7b1f86536c0df66a69c07710e89e0675310f5ba
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-12-14 13:00:26 +0100

    feat: Created configuration modal component

---
+++ src/app/components/modal/configuration/configuration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'configuration-modal',
+  imports: [],
+  templateUrl: './configuration.component.html',
+  styleUrl: './configuration.component.css'
+})
+export class ConfigurationComponent {
+
+}
--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,9 +1,10 @@
 import { Component, computed } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
+import { ConfigurationComponent } from "./configuration/configuration.component";
 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  imports: [ConfigurationComponent],
   standalone: true,
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
