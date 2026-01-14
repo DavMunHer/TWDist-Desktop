1585fc2bc655579a14b0e331ebfb0da65d7f5e39
DavMunHer <davidmunyozh@gmail.com>
2026-01-14 12:35:17 +0100

    feat: created use case for register and added it to the provider

--- src/app/features/auth/application/use-cases/createUser.use-case.ts
+++ src/app/features/auth/application/use-cases/createUser.use-case.ts
@@ -2,13 +2,14 @@ import { Injectable } from "@angular/core";
 import { AuthRepository } from "../../domain/repositories/auth.repository";
 import { Observable } from "rxjs";
 import { User } from "../../domain/entities/user.entity";
+import { RegisterCredentialsDto } from "../../infrastructure/dto/request/register-credentials.dto";
 
 @Injectable()
-export class GetCurrentUserUseCase {
+export class CreateUserUseCase {
   constructor(private authRepository: AuthRepository) {}
 
-  execute(): Observable<User | null> {
+  execute(dto: RegisterCredentialsDto): Observable<User | null> {
     // Just return user, cookie is handled automatically
-    return this.authRepository.getCurrentUser();
+    return this.authRepository.register(dto);
   }
 }
--- src/app/features/auth/auth.providers.ts
+++ src/app/features/auth/auth.providers.ts
@@ -5,6 +5,7 @@ import { LoginUseCase } from "./application/use-cases/login.use-case";
 import { LogoutUseCase } from "./application/use-cases/logout.use-case";
 import { GetCurrentUserUseCase } from "./application/use-cases/getCurrentUser.use-case";
 import { AuthStore } from "./presentation/store/auth.store";
+import { CreateUserUseCase } from "./application/use-cases/createUser.use-case";
 
 export const AUTH_FEATURE_PROVIDERS: Provider[] = [
   { provide: AuthRepository, useClass: HttpAuthRepository },
@@ -12,6 +13,7 @@ export const AUTH_FEATURE_PROVIDERS: Provider[] = [
   LoginUseCase,
   LogoutUseCase,
   GetCurrentUserUseCase,
+  CreateUserUseCase,
   
   AuthStore,
 ];
