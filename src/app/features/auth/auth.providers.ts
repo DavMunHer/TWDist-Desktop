fac1b298e5bd9672b9b2528d76450c7822a603ff
DavMunHer <davidmunyozh@gmail.com>
2026-05-13 17:42:50 +0200

    feat: added necessary use cases for updating username and password

--- src/app/features/auth/auth.providers.ts
+++ src/app/features/auth/auth.providers.ts
@@ -6,6 +6,8 @@ import { LogoutUseCase } from "@features/auth/application/use-cases/logout.use-c
 import { RefreshSessionUseCase } from "@features/auth/application/use-cases/refresh-session.use-case";
 import { GetCurrentUserUseCase } from "@features/auth/application/use-cases/getCurrentUser.use-case";
 import { CreateUserUseCase } from "@features/auth/application/use-cases/createUser.use-case";
+import { UpdateUsernameUseCase } from "@features/auth/application/use-cases/update-username.use-case";
+import { UpdatePasswordUseCase } from "@features/auth/application/use-cases/update-password.use-case";
 import { TokenRefreshCoordinator } from "@features/auth/application/services/token-refresh-coordinator.service";
 import { AuthStore } from "@features/auth/presentation/store/auth.store";
 
@@ -17,6 +19,8 @@ export const AUTH_FEATURE_PROVIDERS: Provider[] = [
   RefreshSessionUseCase,
   GetCurrentUserUseCase,
   CreateUserUseCase,
+  UpdateUsernameUseCase,
+  UpdatePasswordUseCase,
   TokenRefreshCoordinator,
   
   AuthStore,
