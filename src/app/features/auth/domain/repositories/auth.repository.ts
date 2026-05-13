ca7ed936a68a630bcc00a0f6116086bb7474e748
DavMunHer <davidmunyozh@gmail.com>
2026-05-13 17:41:53 +0200

    feat: add updateUsername and updatePassword to auth repository

--- src/app/features/auth/domain/repositories/auth.repository.ts
+++ src/app/features/auth/domain/repositories/auth.repository.ts
@@ -2,12 +2,15 @@ import { Observable } from 'rxjs';
 import { LoginCredentialsDto } from '@features/auth/infrastructure/dto/request/login-credentials.dto';
 import { User } from '@features/auth/domain/entities/user.entity';
 import { RegisterCredentialsDto } from '@features/auth/infrastructure/dto/request/register-credentials.dto';
+import { UpdateUsernameDto } from '@features/auth/infrastructure/dto/request/update-username.dto';
+import { UpdatePasswordDto } from '@features/auth/infrastructure/dto/request/update-password.dto';
 
 export abstract class AuthRepository {
   abstract login(credentials: LoginCredentialsDto): Observable<User>;
   abstract refresh(): Observable<void>;
   abstract logout(): Observable<void>;
   abstract getCurrentUser(): Observable<User | null>;
-
   abstract register(dto: RegisterCredentialsDto): Observable<User>;
+  abstract updateUsername(dto: UpdateUsernameDto): Observable<User>;
+  abstract updatePassword(dto: UpdatePasswordDto): Observable<void>;
 }
