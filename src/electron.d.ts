ed18a3057094e5b1c06b74e18c6dbe89fa0590a0
DavMunHer <davidmunyozh@gmail.com>
2026-05-17 11:50:15 +0200

    fix: added electron type definitions

--- src/electron.d.ts
+++ src/electron.d.ts
@@ -1,10 +1,18 @@
+export interface ElectronAppConfig {
+  apiBaseUrl: string;
+  useBearerAuth?: boolean;
+}
+
 export interface IElectronAPI {
+  getAppConfig?: () => Promise<ElectronAppConfig | null>;
   sendMessage: (msg: string) => void;
   onResponse: (callback: (response: unknown) => void) => void;
 }
 
 declare global {
   interface Window {
-    electronAPI: IElectronAPI;
+    electronAPI?: IElectronAPI;
+    /** Set synchronously by Electron preload when the shell injects config. */
+    __electronRuntimeConfig?: ElectronAppConfig;
   }
 }
